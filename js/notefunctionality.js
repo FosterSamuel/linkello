e1c56156cfa16359c162dd12cf9c041f7aa78f99
Samuel Foster <samuelfosterweb@gmail.com>
2015-10-22 17:01:05 -0700

    Added keyboard functionality. Use ENTER to enter a new link, and ESCAPE (or ENTER again) to leave. Using ENTER with a filled out link will also add it to the list. Cleaned up CSS and HTML as well. git status

--- js/notefunctionality.js
+++ js/notefunctionality.js
@@ -3,9 +3,8 @@
     
     function createNoteHTML(linkIndex) {
         var noteObject = links[linkIndex];
-        var $newNote = "<section class='note' data-id='" +noteObject.id + "'><a target='_blank' href='" + 
-                                                          noteObject.link + "'>" + noteObject.name +  "  </a></section>";
-
+        var $newNote = "<section class='note' data-id='" +noteObject.id + "'><a target='_blank' href='" + noteObject.link + "'>" + noteObject.name +  "  </a></section>";
+        
         return $newNote;
     }
 
@@ -26,22 +25,76 @@
     }
     
 $(document).ready(function() {
-    $("input[name='startnote']").on('click', function() {
-        $('.newnote').toggle();
+    var $startNote = $("input[name='startnote']");
+    var $newNote = $(".newnote");
+    var $addNote = $("input[name='addnote']");
+    
+    var $noteName = $("input[name='name']");
+    var $noteLink = $("input[name='link']");
+    
+    var $section = $('.notes');
+    var newNoteOpen = false;
+    
+    $(document).keypress(function(e) {
+        $focusedElement = $(":focus");
+        
+        if(e.which == 13) {
+            if($focusedElement[0] != $startNote[0] && $focusedElement[0] != $addNote[0]) {
+                if(newNoteOpen) {  
+                    if($noteName.val() != "" && $noteLink.val() != "") {
+                        addNewNote();
+                    } else {
+                        toggleNewNote();   
+                    }
+                } else {
+                    toggleNewNote();   
+                }
+            }
+        }
     });
     
+    $(document).keyup(function(e) {
+        if(e.which == 27 && newNoteOpen) {
+            toggleNewNote();
+        }
+    });
+
     
-    $("input[name='addnote']").on('click', function() {
-        var newNoteName =  $("input[name='name']").val();
-        var newNoteLink = $("input[name='link']").val();
+    $startNote.click(toggleNewNote);
+    $addNote.click(addNewNote);
+    
+    function toggleNewNote() {
+        newNoteOpen = !newNoteOpen;
+        $newNote.slideToggle(function() {
+            if(newNoteOpen) {
+                $noteLink.focus();
+            }   
+        });
+    }
+    
+    function addNewNote() {
+        console.log("called");
+        var newNoteName =  $noteName.val();
+        var newNoteLink = $noteLink.val();
         
-        if(newNoteName != "name" & newNoteLink != "http://") {
+        if(newNoteName != "" && newNoteLink != "") {
+            if(newNoteLink.substr(0, 6).toUpperCase() != "http://".toUpperCase()) {
+                newNoteLink = "http://" + newNoteLink;
+            }
+            
             var newNote = {name: newNoteName, link:newNoteLink, id:linkAmount};
-            var $section = $('.notes');
             
             addNote(newNote);
             $section.append(createNoteHTML(linkAmount-1));
+            
+            $newNote.slideToggle(function() {
+                newNoteOpen = !newNoteOpen;
+                $("input[name='name']").val("");
+                $("input[name='link']").val("");
+            });
+        } else {
+            toggleNewNote();
         }
-    });
+    }
 
 });
